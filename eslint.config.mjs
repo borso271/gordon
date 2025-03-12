5a1ca896d0a6764eb726412deb5f28743517ba24
Federico Borsotti <federicoborsotti@rileys-mbp-2.home>
2025-03-12 12:44:23 +0000

    message

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-expressions": "warn", // ✅ Converts error to warning
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }], // ✅ Ignores unused vars that start with _
+      "@typescript-eslint/no-explicit-any": "off", // ✅ Disables restriction on `any`
+    },
+  },
 ];
 
 export default eslintConfig;

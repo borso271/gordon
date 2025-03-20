bb87d2efd4bc1afd5b15c789305cfb5f0b60aa97
Federico Borsotti <federicoborsotti@federicos-air.home>
2025-03-20 12:04:05 +0000

    added use client

--- src/components/DataDriven/SymbolChart/components/ChartTooltip/index.tsx
+++ src/components/DataDriven/SymbolChart/components/ChartTooltip/index.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, {useState, useEffect} from "react";
 import styles from "./ChartTooltip.module.css";
 import { useScreenSize } from "../../../../../app/context/screenSizeContext";
